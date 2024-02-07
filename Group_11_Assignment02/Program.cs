061f6c57f5b2be7456ebcc4c5ef737fa502980c7
thawng <ttha9806@conestogac.on.ca>
2024-02-06 22:34:41 -0500

    I AM WRITING THE THIS IS TEST FOR TESTING IF IT WORKS OR NTOAdd project files.

---
+++ Group_11_Assignment02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_11_Assignment02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+
+            Console.WriteLine($"Length: {rect.GetLength()}");
+            Console.WriteLine($"Width: {rect.GetWidth()}");
+            Console.WriteLine($"A Perimeter: {rect.GetPerimeter()}");
+            Console.WriteLine($"An Area: {rect.GetArea()}");
+
+            rect.SetLength( 30 );
+            rect.SetWidth( 20 );
+
+            Console.WriteLine("************** Update Rectangle***********");
+
+            Console.WriteLine($"Update Length: {rect.GetLength()}");
+            Console.WriteLine($"Update Width: {rect.GetWidth()}");
+            Console.WriteLine($"Update A Perimeter: {rect.GetPerimeter()}");
+            Console.WriteLine($"Update An Area: {rect.GetArea()}");
+        }
+    }
+}
